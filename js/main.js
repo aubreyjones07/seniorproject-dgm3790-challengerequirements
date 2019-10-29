0ded030db5de89c8e35fe7133822d354df1e7861
Aubrey Jones <aubrey@Deannas-MacBook-Pro.local>
2019-10-29 13:41:26 -0600

    init commit

---
+++ js/main.js
@@ -0,0 +1,31 @@
+/*jslint node:true*/
+"use strict";
+
+/*global alert*/
+
+
+new Vue ({
+    el: '#app',
+    data: {
+        counter: 0, 
+    x: 0,
+    y: 0
+    },
+    methods: {
+        increase: function(step){
+            this.counter += step; 
+
+        }, 
+        updateCoordinates: function(event){
+            this.x = event.clientX; 
+            this.y = event.clientY; 
+
+        }
+        
+
+    }
+    
+    
+
+
+});  
